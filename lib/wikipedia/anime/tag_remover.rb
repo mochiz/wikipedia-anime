e972e450346654b1d9c85dc12f22dbca8f9bf88e
mochiz <mochi.hiro43@gmail.com>
2013-12-22 20:43:58 +0900

    タグ除去に関するメソッドをまとめたタグ除去モジュールを追加した

---
+++ lib/wikipedia/anime/tag_remover.rb
@@ -0,0 +1,66 @@
+
+# タグ除去に関するメソッドをまとめたモジュール
+#
+# Examples
+#
+#   TagRemover.
+#
+module Wikipedia
+  module Anime
+    module TagRemover
+      REFERENCE_TAG     = /<ref.*?<\/ref>|<REF.*?<\/REF>/
+      COMMENT_TAG       = /<!--.*-->/
+      INTERNAL_LINK_TAG = /\[\[|\[\[.*?\||\]\]/
+      SPAN_TAG = /<span.*?>|<\/span>/
+
+      # Public: テキストからタグを除去します。
+      #
+      # string - String
+      #
+      # String を返します。
+      def remove_tag(string)
+        string = remove_reference_tag(string)
+        string = remove_comment_tag(string)
+        string = remove_internal_link_tag(string)
+        string = remove_span_tag(string)
+        string
+      end
+
+      # Publid: 文字列から参照タグを除去します。
+      #
+      # str - String
+      #
+      # String を返します。
+      def remove_reference_tag(str)
+        str.gsub(REFERENCE_TAG, '')
+      end
+
+      # Publid: 文字列からコメントタグを除去します。
+      #
+      # str - String
+      #
+      # String を返します。
+      def remove_comment_tag(str)
+        str.gsub(COMMENT_TAG, '')
+      end
+
+      # Publid: 文字列から内部リンクタグを除去します。
+      #
+      # str - String
+      #
+      # String を返します。
+      def remove_internal_link_tag(str)
+        str.gsub(INTERNAL_LINK_TAG, '')
+      end
+
+      # Publid: 文字列からspanタグを除去します。
+      #
+      # str - String
+      #
+      # String を返します。
+      def remove_span_tag(str)
+        str.gsub(SPAN_TAG, '')
+      end
+    end
+  end
+end
---
+++ spec/wikipedia/anime/tag_remover_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '../../../spec_helper')
+
+describe Wikipedia::Anime::TagRemover do
+  it do
+    @object = Object.new
+    @object.extend(Wikipedia::Anime::TagRemover)
+    expect(@object.remove_reference_tag('hogehoge<ref>fugafuga</ref>')).to eq('hogehoge')
+  end
+end
